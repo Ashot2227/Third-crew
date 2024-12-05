919152d58757d0c800554ef5cf9235f98efb867e
bolrob <belkavas78@mail.ru>
2024-12-05 21:16:07 +0300

    To work

--- FirstTask/src/main/kotlin/application/controller/IncrementController.kt
+++ FirstTask/src/main/kotlin/application/controller/IncrementController.kt
@@ -1,8 +1,5 @@
 package application.controller
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import application.service.IncrementService
 import org.springframework.web.bind.annotation.GetMapping
@@ -13,7 +10,7 @@ class IncrementController(
     private val incrementService: IncrementService
 ) {
     @GetMapping("/{increment}")
-    fun increment(@PathVariable("num") n: Long): Long {
+    fun increment(@PathVariable("increment") n: Long): Long {
         return incrementService.Increment(n)
     }
 }
